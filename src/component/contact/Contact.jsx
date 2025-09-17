a5906b62702a27f5b5f048fe0089086c1bd96a90
Morium Akter <morumakter912003@gmail.com>
2025-09-17 23:01:21 +0600

    add extra code in contact section

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -6,13 +6,24 @@ const Contact = () => {
         <div className="max-w-6xl mx-auto px-6">
             {/* Heading */}
             <div className="text-center mb-12 relative">
-                <h2 className="text-4xl font-extrabold">
+                <h2 className="text-7xl font-extrabold">
                     <span className="text-white">GET IN </span>
                     <span className="text-amber-700">TOUCH</span>
-
                 </h2>
+             </div>
 
-            </div>
+             {/* Grid Layout */}
+             <div className="grid md:grid-cols-2 gap-12 items-start">
+              {/* Left Side */}
+              <div>
+                <h3 className="text-2xl font-bold mb-4">DON'T BE SHY!</h3>
+                <p className="text-gray-300 mb-6">
+                  Feel free to reach out anytime for ideas, projects, or collaboration.
+
+                </p>
+              </div>
+
+             </div>
 
         </div>
     </section>
